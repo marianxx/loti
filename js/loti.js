f6b7ed5938a19c35a65be090d1e96fe030aa8a88
diekus <diekus@gmail.com>
2016-12-13 13:14:49 +0000

    added datasets for browser id

--- js/loti.js
+++ js/loti.js
@@ -12,6 +12,32 @@ function popupMin(){
 }
 
 function popupMax(){
+    var title = document.getElementById('browserTitle');
+    var support = document.getElementById('browserSupport');
+    var b = window.event.srcElement;
+
+    switch (b.dataset.browser) {
+        case 'sb':
+            title.innerText = 'Samsung Internet';
+            break;
+        case 'ff':
+            title.innerText = 'Mozilla Firefox';
+            break;
+        case 'e':
+            title.innerText = 'Microsoft Edge';
+            break;
+        case 'c':
+            title.innerText = 'Google Chrome';
+            break;
+        case 'o':
+            title.innerText = 'Opera';
+            break;
+        case 's':
+            title.innerText = 'Apple Safari';
+            break;
+    }
+    
+    var ss = document.getElementById('screenshot');
     var p = document.getElementById('popup');
     p.style.animationName = 'maximize';
 }
